d670160c68572409dd00a561d2ea6e470cef9012
Ryan Keck <tehrmc08@gmail.com>
2017-02-01 13:14:40 -0700

    Think I got it

--- sol.py
+++ sol.py
@@ -1,3 +1,13 @@
+def rep(coins, curr, last):
+  myCoins = coins[::-1]
+  count = 0
+  for i in range(len(coins)):
+    if myCoins[i] <= last and curr - myCoins[i] >= 0:
+      count = count + rep(coins, curr - myCoins[i], myCoins[i])
+  if curr == 0:
+    return 1
+  return count
+
 def Coin_Combinations(coins, value):
   """
     A Generalized solution to the coins problem provided by project euler #31
@@ -7,4 +17,4 @@ def Coin_Combinations(coins, value):
     returns:
      number of possible combinations to make value with givin coins in "coins"
   """
-  pass
+  return rep(coins, value, coins[len(coins)-1]+1)
